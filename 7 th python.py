b52824f4a49050e373c63edb438246f42d8ffed2
VeerendraViswanadhKantipudi <nani68395@gmail.com>
2026-05-04 13:24:02 +0530

    Add files via upload

---
+++ 7 th python.py
@@ -0,0 +1,112 @@
+'''a= 10
+b= 20
+print(a+b)
+'''
+'''a= 2
+b= 5
+c=a+b
+print(c)'''
+
+#print(5+9)
+
+#run_time input()
+
+'''
+a= int(input("Enter the a Value"))
+b = int(input("Enter the b Value"))
+print(a+b)
+'''
+'''a= float(input("Enter the a Value"))
+b= float(input("Enter the b Value"))
+print(a+b)
+'''
+'''a= bool(input("Enter the a Value"))
+b= bool(input("Enter the b Value"))
+print(a+b)
+'''
+'''a= str(input("datail1"))
+b= str(input("datail2"))
+print((a+" "+b).title())
+'''
+'''a= complex(input("Enter the a Value"))
+b= complex(input("Enter the b Value"))
+print(a+b)'''
+'''a= bool(input("Enter the a Value"))
+b= bool(input("Enter the b Value"))
+c= bool(input("Enter the c Value"))
+print(a+b+c)'''
+
+'''a= int(input("Enter the a Value"))
+b= int(input("Enter the b Value"))
+c= int(input("Enter the c Value"))
+option = int(input("choose the option 1 . add 2 . sub 3 . mult 4 . div"))
+print(a-b)
+print(a*b)
+print(a/b)
+print(a+b)'''
+
+'''name= input()
+print(name)
+'''
+'''a= int(input())
+b= int(input())
+print(a+b)'''
+
+#swapping of two numbers by using four types of numbers
+
+'''a= 10
+b= 20 
+a = a ^ b
+b = a ^ b
+a = a ^ b'''
+'''
+a= 10
+b= 20 
+a, b = b, a'''
+
+'''a= 10
+b= 20
+temp =a
+a = (a+a)
+b= temp
+'''
+'''a = 10
+b = 20
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a, b)'''
+'''
+a= 10
+b = 20
+a = a + b
+b = a - b
+a = a - b
+print("after swapping a=%d, b=%d" %(a,b))
+'''
+'''a=float(input())
+b =float(input())
+a = a + b
+b = a - b
+a = a - b
+print("after swapping a=%d, b=%d" %(a,b))
+'''
+'''
+a=(input())
+b =(input())
+temp= a
+a= b
+b = temp
+
+print("after swapping a=%s, b=%s" %(a,b))
+'''
+'''
+a=int(input("value of a "))
+b =int(input("value of b"))
+a = a + b
+b = a - b
+a = a - b
+print("after swapping a=%d, b=%d" %(a,b))
+'''
