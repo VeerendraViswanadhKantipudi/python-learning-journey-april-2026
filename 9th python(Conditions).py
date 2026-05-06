de9986d8898a3757e740a5121351cc75b9b5a026
Veerendra Viswanadh Kantipudi <nani68395@gmail.com>
2026-05-06 14:55:32 +0530

    Add files via upload

---
+++ 9th python(Conditions).py
@@ -0,0 +1,161 @@
+#if-elif-else conditions
+'''a= 4
+b= 8
+if a<b:
+    print("less")
+elif a>b:
+    print("greater")
+else:
+    print("true")'''
+'''a= 4
+b= 8
+if a==b:
+    print("less")
+elif a>b:
+    print("greater")
+else:
+    print("true")'''
+'''a= 4
+b= 8
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")'''
+'''a= 4
+b= 8
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a==b:
+    print("equal")
+    
+else:
+    print("true")'''
+#logical operators
+'''a= 4
+b= 8
+if a<b and b<a:
+    print("True")
+elif a==b or b!=a:
+    print("Equal")
+elif a%b and a!=b:
+    print("false")
+elif  not a<b :
+    print("Reverse")
+else:
+    print("true")'''
+#assiginment operators
+'''a= 4
+b= 8
+if a=b:
+    print("result")
+elif a+=b:
+    print("greater")
+elif a=b :
+    print("equal")
+elif a//=b:
+    print("answer")
+else:
+    print("true")'''
+#membership operators
+'''a=[1,2,3,4,5,6]
+b= [8,19,13,14,15,16]
+if 13 in b :
+    print("True")
+elif 2 not in a:
+    print("Again")
+else:
+    print("ok")'''
+#Identity operator
+'''list_a = [1, 2, 3]
+list_b = [1, 2, 3]
+list_c = list_a  # c points to the same object as a
+
+target = list_c
+
+if target is list_b:
+    print("Target is the same object as list_b.")
+    
+elif target is list_a:
+    print("Target is the exact same object as list_a.")
+    # This will print because list_c (target) and list_a share the same memory ID.
+
+else:
+    print("Target is a unique object.")
+'''
+
+# multiple if
+'''a= 8
+b= 10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not  equal")'''
+'''a= 8
+b= 10
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not  equal")'''
+#logical operators
+'''a= 8
+b= 10
+if a<b and b>a:
+    print("True")
+if b==a or a==b:
+    print("ok")
+if not a==b :
+    print("not  equal")'''
+#membership operators
+'''a=[1,2,3,4,5]
+if 2 in a:
+    print("yes")
+if 10 not in a:
+    print("no")'''
+#identity operators
+'''a= int(input())
+b= bool(input())
+if type(a) is  int:
+    print("true")
+if type(b) is not int:
+    print("false")'''
+#nested-if conditions in this if only first condition is true then only it will go to the another condition
+'''a= 5
+b= 10
+if  a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+'''a= 5
+b= 10
+if  a==b:
+    print("less")
+    if b==a:
+        print("greater")'''
+'''a= 9
+b= 11
+if  a>b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")
+    '''
+# in a if statement only one else will work if we give more than 1 then the lates else will only work below is the example
+'''a= 5
+b= 10
+if  a==b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+        print("ok ")
+else:
+    print("no")'''
