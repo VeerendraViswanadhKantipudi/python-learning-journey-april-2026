f02206e0332c663fd7c2ac370c821137163d5f20
Veerendra Viswanadh Kantipudi <nani68395@gmail.com>
2026-05-08 13:15:21 +0530

    Add files via upload

---
+++ 10th python (loops).py
@@ -0,0 +1,63 @@
+#loops
+#there are six types of loops are for, loop, while, range, break, continue, pass
+#for(he for loop is used for iterating over a sequence (like a list, tuple, dictionary, set, or string). It’s essentially an "iterator" engine.)
+#i is 
+'''a=[10,20,30,40,50]#list
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+a=[10,30,40,60,70]
+for i in a:
+    print(i,end="[],")
+
+'''a=(3,4,5,6,7)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+    print(i,end=",")
+    '''
+
+'''a= {4,5,6,7,8,9}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+'''d={"year":2026,"month":5,"date":4}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.values():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.items():
+    print(i)
+    print(type(d))
+    print(type(i))'''
+
+'''a = "codegnan"
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=["apple","bannana","orange"]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+'''a=[3,5.6,"Veerendra",6+6j,True,False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
---
+++ 11 th Python(loops).py
@@ -0,0 +1,180 @@
+#while loop
+'''a= 10
+while a<1:
+    print("true")'''
+
+
+
+'''a= 20
+while a>1:
+    print(a)'''
+
+
+'''a= 20
+while a>1:
+    print(a)
+    a=a-1'''
+
+
+
+'''a= 30
+while a>1:
+    a=a-1
+    print(a)'''
+
+
+
+'''a= 20
+while a>4:
+    a=a-1
+print(a)'''
+
+
+'''a= 10
+while a>1:
+    print(a)
+    a+=1'''
+
+
+'''a= 1
+while a<10:
+    print(a)
+    a+=1'''
+
+#while true
+
+'''while True:
+    age = int(input("age="))
+    if age>=18 :
+        print("They are eligible fro voting")
+    else:
+        print("they are not elogible")'''
+
+
+'''while True:
+    n= int(input("number"))
+    if n%2==0 :
+        print("even")
+    else:
+        print("odd")'''
+
+'''while True:
+    vowels = ["a","e","i","o","u"]
+    n= input()
+    if n in vowels:
+        print("vowels")
+    else:
+        print("consonaunt")'''
+
+'''while True:
+    name = "Veerendra"
+    n=input()
+    if n==name:
+        print("welcome Veerendra")
+    else:
+        print("Welcome Guest")'''
+# range()
+# range has start, stop, step
+'''for i in range(10):
+    print(i)'''
+#start
+#stop
+
+'''for i in range(15,26):
+    print(i)'''
+
+
+#step
+'''for i in range(0,24,5):
+    print(i)'''
+
+
+
+# break
+
+'''a= 30
+while a>5:
+    print(a)
+    a=a-1
+    if a==15:
+        break
+    print(a)'''
+
+
+'''a= 30
+while a>5:
+    print(a)
+    a=a-1
+    if a==15:
+        break
+print(a)'''
+
+'''for i in range(19):
+    if i==10:
+        break
+    print(i)'''
+
+
+
+'''a= "python"
+if a=="h":
+    break
+    print(a)'''#error
+
+
+
+'''a= "python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+#continue
+
+'''a=20
+while a>1:
+    print(a)
+    a=a-1
+    if a== 10:
+        continue'''
+
+
+
+'''a=20
+while a>1:
+    a=a-1
+    if a== 10:
+        continue
+    print(a)'''
+
+
+'''for i in range(15):
+    if i ==12:
+        continue
+    print(i)'''
+
+
+'''a= "python"
+for i in a:
+    if i =="t":
+        continue
+    print(i)'''
+
+
+#pass( is nothing but a place holder)it does nothing it was mainly used in opps
+
+'''a= 10
+while a>1:
+    print(a)
+    a=a-1
+    if a==5:
+        pass'''
+
+
+        
+for i in range(5):
+    if i==2:
+        pass
+    print(i)
+
+
